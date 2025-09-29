33e2391a76c20bd272d4b8fff126c343efdfcf15
klewy <cigankov807@gmail.com>
2025-09-29 11:38:08 +0300

    Started working on a tree model

--- models/filestablemodel.cpp
+++ models/filestablemodel.cpp
@@ -3,190 +3,190 @@
 #include <libtorrent/download_priority.hpp>
 #include "priority.h"
 
-FileTableModel::FileTableModel(QObject *parent) : QAbstractTableModel(parent)
-{
-}
+// FileTableModel::FileTableModel(QObject *parent) : QAbstractTableModel(parent)
+// {
+// }
 
-Qt::ItemFlags FileTableModel::flags(const QModelIndex &index) const
-{
-    if (index.column() == static_cast<int>(FileFields::PRIORITY)) {
-        return QAbstractItemModel::flags(index) | Qt::ItemIsEditable;
-    }
-    return QAbstractItemModel::flags(index);
-}
+// Qt::ItemFlags FileTableModel::flags(const QModelIndex &index) const
+// {
+//     if (index.column() == static_cast<int>(FileFields::PRIORITY)) {
+//         return QAbstractItemModel::flags(index) | Qt::ItemIsEditable;
+//     }
+//     return QAbstractItemModel::flags(index);
+// }
 
-QVariant FileTableModel::data(const QModelIndex &index, int role) const
-{
-    if (role < Qt::UserRole) {
-        if (role == Qt::DisplayRole) {
-            auto& file =  m_files[index.row()];
-            switch (static_cast<FileFields>(index.column())) {
-                case FileFields::STATUS: {
-                    return {file.isEnabled};
-                }
-                case FileFields::FILENAME: {
-                    return {file.filename};
-                }
-                case FileFields::PROGRESS: {
-                    auto ratio = static_cast<double>(file.downloaded) / file.filesize;
-                    ratio = std::round(ratio * 100.0) / 100.0;
-                    return {ratio};
-                }
-                case FileFields::DOWNLOADED: {
-                    return {utils::bytesToHigher(file.downloaded)};
-                }
-                case FileFields::FILESIZE: {
-                    return {utils::bytesToHigher(file.filesize)};
-                }
-                case FileFields::PRIORITY: {
-                    auto priorityToString = [](int const priority) -> QString {
-                        switch (priority) {
-                            case lt::dont_download: {
-                                return {Priorities::DONT_DOWNLOAD};
-                            }
-                            case lt::default_priority: {
-                                return {Priorities::DEFAULT};
-                            }
-                            case lt::low_priority: {
-                                return {Priorities::LOW};
-                            }
-                            case lt::top_priority: {
-                                return {Priorities::HIGH};
-                            }
-                            default: {
-                                throw std::runtime_error("Cant be here");
-                            }
-                        }
-                    };
-                    return {priorityToString(file.priority)};
-                }
-                default: {
-                    throw std::runtime_error("TF u doing here?");
-                }
-            }
-        }
-    }
-    return {};
-}
+// QVariant FileTableModel::data(const QModelIndex &index, int role) const
+// {
+//     if (role < Qt::UserRole) {
+//         if (role == Qt::DisplayRole) {
+//             auto& file =  m_files[index.row()];
+//             switch (static_cast<FileFields>(index.column())) {
+//                 case FileFields::STATUS: {
+//                     return {file.isEnabled};
+//                 }
+//                 case FileFields::FILENAME: {
+//                     return {file.filename};
+//                 }
+//                 case FileFields::PROGRESS: {
+//                     auto ratio = static_cast<double>(file.downloaded) / file.filesize;
+//                     ratio = std::round(ratio * 100.0) / 100.0;
+//                     return {ratio};
+//                 }
+//                 case FileFields::DOWNLOADED: {
+//                     return {utils::bytesToHigher(file.downloaded)};
+//                 }
+//                 case FileFields::FILESIZE: {
+//                     return {utils::bytesToHigher(file.filesize)};
+//                 }
+//                 case FileFields::PRIORITY: {
+//                     auto priorityToString = [](int const priority) -> QString {
+//                         switch (priority) {
+//                             case lt::dont_download: {
+//                                 return {Priorities::DONT_DOWNLOAD};
+//                             }
+//                             case lt::default_priority: {
+//                                 return {Priorities::DEFAULT};
+//                             }
+//                             case lt::low_priority: {
+//                                 return {Priorities::LOW};
+//                             }
+//                             case lt::top_priority: {
+//                                 return {Priorities::HIGH};
+//                             }
+//                             default: {
+//                                 throw std::runtime_error("Cant be here");
+//                             }
+//                         }
+//                     };
+//                     return {priorityToString(file.priority)};
+//                 }
+//                 default: {
+//                     throw std::runtime_error("TF u doing here?");
+//                 }
+//             }
+//         }
+//     }
+//     return {};
+// }
 
-bool FileTableModel::setData(const QModelIndex &index, const QVariant &value, int role)
-{
-    if (role == Qt::EditRole) {
-        auto& file = m_files[index.row()];
-        switch (static_cast<FileFields>(index.column())) {
-        case FileFields::STATUS: {
-            file.isEnabled = value.toBool();
-            emit statusChanged(file.id, file.isEnabled);
-            break;
-        }
-        case FileFields::PRIORITY: {
-            qDebug() << "Priority set data";
-            file.priority = value.toInt();
-            emit priorityChanged(file.id, file.priority);
-            break;
-        }
-        // ...
-        default: {
-            throw std::runtime_error("Something is wrong");
-            break;
-        }
-        }
-        return true;
-    }
-    return false;
-}
+// bool FileTableModel::setData(const QModelIndex &index, const QVariant &value, int role)
+// {
+//     if (role == Qt::EditRole) {
+//         auto& file = m_files[index.row()];
+//         switch (static_cast<FileFields>(index.column())) {
+//         case FileFields::STATUS: {
+//             file.isEnabled = value.toBool();
+//             emit statusChanged(file.id, file.isEnabled);
+//             break;
+//         }
+//         case FileFields::PRIORITY: {
+//             qDebug() << "Priority set data";
+//             file.priority = value.toInt();
+//             emit priorityChanged(file.id, file.priority);
+//             break;
+//         }
+//         // ...
+//         default: {
+//             throw std::runtime_error("Something is wrong");
+//             break;
+//         }
+//         }
+//         return true;
+//     }
+//     return false;
+// }
 
-QVariant FileTableModel::headerData(int section, Qt::Orientation orientation, int role) const
-{
-    if (role != Qt::DisplayRole) {
-        return {};
-    }
+// QVariant FileTableModel::headerData(int section, Qt::Orientation orientation, int role) const
+// {
+//     if (role != Qt::DisplayRole) {
+//         return {};
+//     }
 
-    if (orientation == Qt::Horizontal) {
-        switch (static_cast<FileFields>(section)) {
-            case FileFields::STATUS: {
-                return {tr("Status")};
-            }
-            case FileFields::FILENAME: {
-                return {tr("Name")};
-            }
-            case FileFields::PROGRESS: {
-                return {tr("Progress")};
-            }
-            case FileFields::DOWNLOADED: {
-                return {tr("Downloaded")};
-            }
-            case FileFields::FILESIZE: {
-                return {tr("Size")};
-            }
-            case FileFields::PRIORITY: {
-                return {tr("Priority")};
-            }
-            default: {
-                throw std::runtime_error("TF u doing here?");
-            }
-        }
-    } else {
-        return {section + 1};
-    }
-}
+//     if (orientation == Qt::Horizontal) {
+//         switch (static_cast<FileFields>(section)) {
+//             case FileFields::STATUS: {
+//                 return {tr("Status")};
+//             }
+//             case FileFields::FILENAME: {
+//                 return {tr("Name")};
+//             }
+//             case FileFields::PROGRESS: {
+//                 return {tr("Progress")};
+//             }
+//             case FileFields::DOWNLOADED: {
+//                 return {tr("Downloaded")};
+//             }
+//             case FileFields::FILESIZE: {
+//                 return {tr("Size")};
+//             }
+//             case FileFields::PRIORITY: {
+//                 return {tr("Priority")};
+//             }
+//             default: {
+//                 throw std::runtime_error("TF u doing here?");
+//             }
+//         }
+//     } else {
+//         return {section + 1};
+//     }
+// }
 
-QHash<int, QByteArray> FileTableModel::roleNames() const
-{
-    return {};
-}
+// QHash<int, QByteArray> FileTableModel::roleNames() const
+// {
+//     return {};
+// }
 
-void FileTableModel::setFiles(const QList<File> &files)
-{
-    qDebug() << "Setting " << files.size();
-    // TODO: Optimize it, since it is suitable for torrents list, but i guess i can do it easier for (almost) static files
-    QHash<int, int> newTrackersMap; // key - index in trackers
-    QHash<int, int> oldTrackersMap; // key - index in m_trackers
+// void FileTableModel::setFiles(const QList<File> &files)
+// {
+//     qDebug() << "Setting " << files.size();
+//     // TODO: Optimize it, since it is suitable for torrents list, but i guess i can do it easier for (almost) static files
+//     QHash<int, int> newTrackersMap; // key - index in trackers
+//     QHash<int, int> oldTrackersMap; // key - index in m_trackers
 
-    // Cache trackers
-    for (int i = 0; i < files.size(); ++i) {
-        newTrackersMap.insert(files[i].id, i);
-    }
-    for (int i = 0; i < m_files.size(); ++i) {
-        oldTrackersMap.insert(m_files[i].id, i);
-    }
+//     // Cache trackers
+//     for (int i = 0; i < files.size(); ++i) {
+//         newTrackersMap.insert(files[i].id, i);
+//     }
+//     for (int i = 0; i < m_files.size(); ++i) {
+//         oldTrackersMap.insert(m_files[i].id, i);
+//     }
 
-    for (int i = 0; i < files.size(); ++i) {
-        auto const newKey = files[i].id;
-        if (oldTrackersMap.contains(newKey)) {
-            // Tracker exists, check for updates
-            int row = oldTrackersMap[newKey];
-            auto& oldFile = m_files[row];
+//     for (int i = 0; i < files.size(); ++i) {
+//         auto const newKey = files[i].id;
+//         if (oldTrackersMap.contains(newKey)) {
+//             // Tracker exists, check for updates
+//             int row = oldTrackersMap[newKey];
+//             auto& oldFile = m_files[row];
 
-            oldFile.filename = files[i].filename; // Since files can be renamed
-            oldFile.downloaded = files[i].downloaded;
-            oldFile.priority = files[i].priority;
-        } else {
-            int row = m_files.size();
-            beginInsertRows(QModelIndex(), row, row);
-            m_files.append(files[i]);
-            endInsertRows();
-        }
-    }
+//             oldFile.filename = files[i].filename; // Since files can be renamed
+//             oldFile.downloaded = files[i].downloaded;
+//             oldFile.priority = files[i].priority;
+//         } else {
+//             int row = m_files.size();
+//             beginInsertRows(QModelIndex(), row, row);
+//             m_files.append(files[i]);
+//             endInsertRows();
+//         }
+//     }
 
-    // Handle deletions
-    for (int i = m_files.size() - 1; i >= 0; --i) {
-        auto const key = m_files[i].id;
-        if (!newTrackersMap.contains(key)) {
-            beginRemoveRows(QModelIndex(), i, i);
-            m_files.removeAt(i);
-            endRemoveRows();
-        }
-    }
+//     // Handle deletions
+//     for (int i = m_files.size() - 1; i >= 0; --i) {
+//         auto const key = m_files[i].id;
+//         if (!newTrackersMap.contains(key)) {
+//             beginRemoveRows(QModelIndex(), i, i);
+//             m_files.removeAt(i);
+//             endRemoveRows();
+//         }
+//     }
 
-    if (!m_files.isEmpty()) {
-        emit dataChanged(index(0, 1), index(m_files.size() - 1, columnCount() - 1));
-    }
-}
+//     if (!m_files.isEmpty()) {
+//         emit dataChanged(index(0, 1), index(m_files.size() - 1, columnCount() - 1));
+//     }
+// }
 
-void FileTableModel::clearFiles()
-{
-    beginResetModel();
-    m_files.clear();
-    endResetModel();
-}
+// void FileTableModel::clearFiles()
+// {
+//     beginResetModel();
+//     m_files.clear();
+//     endResetModel();
+// }
--- models/filestablemodel.h
+++ models/filestablemodel.h
@@ -4,44 +4,186 @@
 #include <QWidget>
 #include <QAbstractTableModel>
 #include "file.h"
+#include <libtorrent/download_priority.hpp>
+#include "priority.h"
+#include "utils.h"
 
 namespace Ui {
 class FilesTableModel;
 }
 
-class FileTableModel : public QAbstractTableModel
-{
-    Q_OBJECT
+class FileItem {
+    enum FileType {
+        TYPE_DIRECTORY,
+        TYPE_FILE
+    };
 public:
-    explicit FileTableModel(QObject *parent = nullptr);
-
-    int rowCount(const QModelIndex& index = QModelIndex{}) const override {
-        return m_files.size();
+    FileItem(int type, FileItem* parent = nullptr) : m_fileType((FileType)type), m_parent(parent) {}
+    ~FileItem() {
+        qDeleteAll(m_children);
     }
-    int columnCount(const QModelIndex& index = QModelIndex{}) const override {
+
+    void addFile(FileItem* file) {
+        m_children.append(file);
+    }
+
+    int childCount() const {
+        return m_children.count();
+    }
+
+    QList<FileItem*> children() const {
+        return m_children;
+    }
+
+    FileItem* parent() const {
+        return m_parent;
+    }
+    void setParent(FileItem* newParent) {
+        m_parent = newParent;
+    }
+
+    QVariant getValue(int col) const {
+        switch (static_cast<FileFields>(col)) {
+            case FileFields::STATUS: {
+                return {file.isEnabled};
+            }
+            case FileFields::FILENAME: {
+                return {file.filename};
+            }
+            case FileFields::PROGRESS: {
+                auto ratio = static_cast<double>(file.downloaded) / file.filesize;
+                ratio = std::round(ratio * 100.0) / 100.0;
+                return {ratio};
+            }
+            case FileFields::DOWNLOADED: {
+                return {utils::bytesToHigher(file.downloaded)};
+            }
+            case FileFields::FILESIZE: {
+                return {utils::bytesToHigher(file.filesize)};
+            }
+            case FileFields::PRIORITY: {
+                auto priorityToString = [](int const priority) -> QString {
+                    switch (priority) {
+                        case lt::dont_download: {
+                            return {Priorities::DONT_DOWNLOAD};
+                        }
+                        case lt::default_priority: {
+                            return {Priorities::DEFAULT};
+                        }
+                        case lt::low_priority: {
+                            return {Priorities::LOW};
+                        }
+                        case lt::top_priority: {
+                            return {Priorities::HIGH};
+                        }
+                        default: {
+                            throw std::runtime_error("Cant be here");
+                        }
+                    }
+                };
+                return {priorityToString(file.priority)};
+            }
+            default: {
+                throw std::runtime_error("TF u doing here?");
+            }
+        }
+    }
+private:
+    FileType m_fileType;
+    FileItem* m_parent{nullptr};
+    QList<FileItem*> m_children;
+    File file; // Only if is a file
+};
+
+class FileTreeModel : public QAbstractItemModel {
+    Q_OBJECT
+
+    FileItem* objByIndex(const QModelIndex& index) const {
+        if (!index.isValid()) return m_rootItem;
+        return static_cast<FileItem*>(index.internalPointer());
+    }
+public:
+    explicit FileTreeModel(QObject* parent = nullptr) {
+        m_rootItem = new FileItem{0, nullptr};
+    }
+
+    QModelIndex index(int row, int column, const QModelIndex &parent) const override {
+        if (!hasIndex(row, column, parent)) return QModelIndex{};
+
+
+        auto* obj = objByIndex(parent);
+        return createIndex(row, column, obj->children().at(row));
+    }
+    QModelIndex parent(const QModelIndex &child) const override {
+        auto* childObj = objByIndex(child);
+        auto* parentObj = childObj->parent();
+
+        if (parentObj == m_rootItem) return QModelIndex{};
+
+        auto* grandparentObj = parentObj->parent();
+        int row = grandparentObj->children().indexOf(parentObj);
+        return createIndex(row, 0, parentObj);
+    }
+
+    int rowCount(const QModelIndex &parent) const override {
+        return objByIndex(parent)->childCount();
+    }
+    int columnCount(const QModelIndex &parent) const override {
         return FILE_FIELD_COUNT;
     }
+    QVariant data(const QModelIndex &index, int role) const override {
+        if (!index.isValid()) return QVariant{};
 
-    Qt::ItemFlags flags(const QModelIndex &index) const override;
+        if (role == Qt::DisplayRole) {
+            FileItem* obj = objByIndex(index);
+            return obj->getValue(index.column());
+        }
+        return QVariant{};
+    }
 
-    QVariant data(const QModelIndex& index = QModelIndex{}, int role = Qt::DisplayRole) const override;
-    bool setData(const QModelIndex& index = QModelIndex{}, const QVariant& value = QVariant{}, int role = Qt::EditRole) override; // Probably will be needed for editing (enabled, disabled, priority)
+    void addItem(FileItem* item, const QModelIndex& parentIndex) {
+        beginInsertRows(parentIndex, rowCount(parentIndex), rowCount(parentIndex));
+        item->setParent(objByIndex(parentIndex));
+        endInsertRows();
+    }
 
-    QVariant headerData(int section, Qt::Orientation, int role = Qt::DisplayRole) const override;
-    QHash<int, QByteArray> roleNames() const override;
-
-
-    int getFileId(int index) const { return m_files[index].id; }
-    bool getIsEnabled(int index) const { return m_files[index].isEnabled; }
-
-    void setFiles(const QList<File>& files);
-    void clearFiles();
-signals:
-    void statusChanged(int index, bool value);
-    void priorityChanged(int index, int priority);
 private:
-    QList<File> m_files;
+    FileItem* m_rootItem;
 };
+
+// class FileTableModel : public QAbstractTableModel
+// {
+//     Q_OBJECT
+// public:
+//     explicit FileTableModel(QObject *parent = nullptr);
+
+//     int rowCount(const QModelIndex& index = QModelIndex{}) const override {
+//         return m_files.size();
+//     }
+//     int columnCount(const QModelIndex& index = QModelIndex{}) const override {
+//         return FILE_FIELD_COUNT;
+//     }
+
+//     Qt::ItemFlags flags(const QModelIndex &index) const override;
+
+//     QVariant data(const QModelIndex& index = QModelIndex{}, int role = Qt::DisplayRole) const override;
+//     bool setData(const QModelIndex& index = QModelIndex{}, const QVariant& value = QVariant{}, int role = Qt::EditRole) override; // Probably will be needed for editing (enabled, disabled, priority)
+
+//     QVariant headerData(int section, Qt::Orientation, int role = Qt::DisplayRole) const override;
+//     QHash<int, QByteArray> roleNames() const override;
+
+
+//     int getFileId(int index) const { return m_files[index].id; }
+//     bool getIsEnabled(int index) const { return m_files[index].isEnabled; }
+
+//     void setFiles(const QList<File>& files);
+//     void clearFiles();
+// signals:
+//     void statusChanged(int index, bool value);
+//     void priorityChanged(int index, int priority);
+// private:
+//     QList<File> m_files;
+// };
 
 
 #endif // FILESTABLEMODEL_H
